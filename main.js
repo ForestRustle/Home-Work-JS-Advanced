309f197cf0728313ae915bbaacd8f4d6c623509f
ForestRustle <fenix66690@mail.ru>
2025-04-10 23:32:59 +0300

    fix- Добавлен вывод ошибки и регистр параметра

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Поддерживаемые типы: D4, D6, D8, D10, D12, D16, D20.
 function randomDice(dice) {
+  const diceLower = dice.toLowerCase()
   const dices = {
     d4: 4,
     d6: 6,
@@ -9,13 +10,14 @@ function randomDice(dice) {
     d16: 16,
     d20: 20,
   };
-  if (dices[dice]) {
-    return Math.floor((Math.random() * dices[dice] + 1));
+  if (dices[diceLower]) {
+    return Math.floor((Math.random() * dices[diceLower] + 1));
   }
   else {
-    console.log('Неверное значение');
-    return;
+    return `Ошибка: Неверный тип дайса. Поддерживаемые типы: ${Object.keys(dices).join(', ')}`;
   }
 }
 
-console.log(randomDice('d6'));
+console.log(randomDice('D6'));
+console.log(randomDice('d8'));
+console.log(randomDice('d'));
