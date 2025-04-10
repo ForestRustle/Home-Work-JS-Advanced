4cd1d878d1b3a15c1773635a6723e048e7ddfa13
ForestRustle <fenix66690@mail.ru>
2025-04-10 23:06:41 +0300

    Добавлена функция для генерации даты

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 'use strict';
+function randomDate(min, max) {
+  const minDate = new Date(min);
+  const maxDate = new Date(max);
+  const randomTime = minDate.getTime() + Math.floor(Math.random() * (maxDate.getTime() - minDate.getTime()));
 
+  const date = new Date(randomTime);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
 function validAge(birthday) {
   const nowDate = new Date();
   const userBirthday = new Date(birthday).getTime();
   const userAge = Math.floor((nowDate - userBirthday) / (1000 * 60 * 60 * 24 * 365));
-  return userAge > 14 ? true : false;
+  return userAge >= 14;
 }
 
-console.log(validAge('2022-01-01'));
-console.log(validAge('2000-01-01'));
+const result = randomDate('1990-01-01', '2025-01-01');
+
+console.log(validAge(result));
