fcbe041c69b0e4b8c109627d968a4287f37428ae
ForestRustle <fenix66690@mail.ru>
2025-05-23 01:15:00 +0300

    fix / Добавлена стилистика и фукнционал очистки сообщения

--- 14-user-task/index.js
+++ 14-user-task/index.js
@@ -3,7 +3,27 @@
 import { Task} from './modules/task.js'
 import { User } from './modules/user.js';
 
-const task = new Task('some one');
-const user = new User(task);
+const pagesElements = {
+  textButton: document.querySelector('.button-text'),
+  input: document.querySelector('.message'),
+  userMessage: document.querySelector('.user-message'),
+  clearMessage: document.querySelector('.clear-text')
+}
 
-user.do()
+pagesElements.textButton.addEventListener('click', () => {
+  const message = pagesElements.input.value;
+  if (message.trim() === '') {
+    console.log('Сообщение пустой');
+    return;
+  }
+  const task = new Task(message);
+  const user = new User(task);
+
+  user.do();
+  pagesElements.userMessage.innerHTML = message
+  pagesElements.input.value = "";
+});
+
+pagesElements.clearMessage.addEventListener('click', () => {
+  pagesElements.userMessage.innerHTML = ""
+})
